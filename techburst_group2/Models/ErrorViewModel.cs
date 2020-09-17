82f40ea72c82e7c7667ce8d76dce4172b0b17845
Luca Bulles <lucabulles@gmail.com>
2020-09-17 11:56:19 +0200

    First commit

---
+++ techburst_group2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace techburst_group2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
