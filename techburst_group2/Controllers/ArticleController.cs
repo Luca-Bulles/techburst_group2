8a38e0f0ed4b76e0c75733fd0d66d9a738fd9e55
Calin Peters <calinpeters98@hotmail.nl>
2020-09-24 09:59:42 +0200

    Added Article controller.

---
+++ techburst_group2/Controllers/ArticleController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace techburst_group2.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Article(int id)
+        {
+            return View();
+        }
+
+        public IActionResult CreateArticle()
+        {
+            return View();
+        }
+
+        public IActionResult EditArticle(int id)
+        {
+            return View();
+        }
+
+        public IActionResult DeleteArticle(int id)
+        {
+            return View();
+        }
+    }
+}
