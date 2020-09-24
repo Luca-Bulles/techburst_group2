f3be365debda5814d975a581e2b0ccf3609c578d
markgoertz <58692211+markgoertz@users.noreply.github.com>
2020-09-24 12:09:56 +0200

    Commitment DAL laag error fixed

---
+++ techburst_Data Access Layer/DTO/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using techburst_Interface;
+
+namespace techburst_Data_Access_Layer.DTO
+{
+    public class Article: IArticle
+    {
+       public int ArticleID { get; set; }
+        public int AccountID { get; set; }
+        public string Title { get; set; }
+        public string ArticleText { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool Draft { get; set; }
+        public DateTime LastEdited { get; set; }
+        public string Images { get; set; }
+    }
+}
