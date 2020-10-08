f923fd8cfb9310e4292d87c54443426dd3a8f55d
Calin Peters <calinpeters98@hotmail.nl>
2020-10-08 09:30:28 +0200

    Added factory project; removed unit of works.

--- techburst_group2/Controllers/HomeController.cs
+++ techburst_group2/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace techburst_group2.Controllers
             List<ArticleModel> articles = new List<ArticleModel>();
             for (int i = 0; i < 9; i++)
             {
-                ArticleModel article = new ArticleModel("test", "testbody", "Calin Peters");
+                ArticleModel article = new ArticleModel("Sample Title", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "Calin Peters");
                 articles.Add(article);
 
             }
---
+++ techburst_group2/wwwroot/js/carousel.js
@@ -0,0 +1,15 @@
+﻿/*Downloaded from https://www.codeseek.co/jonathanlcarroll/netflix-style-carousel-aNgRBb */
+$('#controlR').click(function () {
+    event.preventDefault();
+    $('#content').animate({
+        marginLeft: "-=400px"
+    }, "fast");
+});
+
+$('#controlL').click(function () {
+    event.preventDefault();
+    $('#content').animate({
+        marginLeft: "+=400px"
+    }, "fast");
+});
+
