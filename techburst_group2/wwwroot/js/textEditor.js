6bb5ce3708237099f4411d295da00a1824f59045
Calin Peters <calinpeters98@hotmail.nl>
2020-11-30 12:16:00 +0100

    Added comments for clarification in the text editor Javascript.

--- techburst_group2/wwwroot/js/textEditor.js
+++ techburst_group2/wwwroot/js/textEditor.js
@@ -1,39 +1,47 @@
-﻿var toolbarOptions = [
-    ['bold', 'italic', 'underline', 'strike'],
-    ['blockquote', 'code-block'],
-    [{ 'header': 1 }, { 'header': 2 }],
-    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-    [{ 'script': 'sub' }, { 'script': 'super' }],
-    [{ 'indent': '-1' }, { 'indent': '+1' }],
-    [{ 'direction': 'rtl' }],
-    [{ 'size': ['small', false, 'large', 'huge'] }],
-    ['link', 'image', 'video', 'formula'],
-    [{ 'color': [] }, { 'background': [] }],
-    [{ 'font': [] }],
-    [{ 'align': [] }]
+﻿
+//Configure the visible toolbar options for the text-editor:
+var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],          //text markup like bold, italic.
+    ['blockquote', 'code-block'],                       //markup for codesnippets or quotes.
+    [{ 'header': 1 }, { 'header': 2 }],                 //text header types.
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],      //list types.
+    [{ 'script': 'sub' }, { 'script': 'super' }],       //script options.
+    [{ 'indent': '-1' }, { 'indent': '+1' }],           //text identation options.
+    [{ 'direction': 'rtl' }],                       
+    [{ 'size': ['small', false, 'large', 'huge'] }],    //text sizes.
+    ['link', 'image', 'video', 'formula'],              //embedded data (videos, pictures, etc.)
+    [{ 'color': [] }, { 'background': [] }],            //text & background color.
+    [{ 'font': [] }],                                   //font types.
+    [{ 'align': [] }]                                   //text aligning.
 ];
-var options = {
-    debug: 'info',
-    modules: {
-        toolbar: toolbarOptions
-    },
-    readOnly: false,
-    theme: 'snow'
-};
-var quill = new Quill('#editor', options);
 
-var form = document.querySelector('#create');
-form.onsubmit = function () {
+//Set options for debugging, text editor themes, etc.
+var options = {
+    debug: 'info',                                      //set debugging options.
+    modules: {                                          //array for modules.
+        toolbar: toolbarOptions                         //set the toolbar options.
+    },
+    readOnly: false,                                    //set readOnly property.
+    theme: 'snow'                                       //set theme for text editor.
+};
+var quill = new Quill('#editor', options);              //instantiate the text editor within the specified div ID (#editor) and the options as defined in the above variable.
+
+var form = document.querySelector('#create');           //get form from View.
+
+//This function gets the content from an input field with the name content from the View, and then calls getQuillText in order to set the value for the hidden input field in the View.
+form.onsubmit = function () {                           
     var content = document.querySelector('input[name=content]');
     content.value = getQuillText();
-    alert(content.value);
+    alert(content.value); //Alert is for debugging purposes only, and can be safely removed later on.
 }
 
+//Initialize variables for getQuillText function.
 var editors = {};
 editors[editor] = quill;
 
+//This function gets the text from the Quill Editor and returns it.
 function getQuillText() {
-    var quill = editors[editor];
-    var text = quill.getText();
+    var quillEditor = editors[editor];
+    var text = quillEditor.getText();
     return text;
 }
