380151abb4fd0891125502f9a96badb0ba450fa4
Calin Peters <calinpeters98@hotmail.nl>
2020-12-07 15:17:28 +0100

    Article text gets displayed more clearly now (no html tags or headers).

--- techburst_group2/Utilities/ArticleEncoder.cs
+++ techburst_group2/Utilities/ArticleEncoder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text.RegularExpressions;
 
 namespace techburst_group2.Utilities
 {
@@ -12,6 +13,16 @@ namespace techburst_group2.Utilities
         public static string DecodeArticleText(string articleText)
         {
             return WebUtility.HtmlDecode(articleText).ToString();
+        }
+
+        public static string ParseHtmlTags(string articleText)
+        {
+            return Regex.Replace(articleText, @"<(.|\n)*?>", string.Empty);
+        }
+
+        public static string ParseHeaderContent(string articleText)
+        {
+            return Regex.Replace(articleText, @"\s?<h[0-9]>[^<]+</h[0-9]>\s?", string.Empty);
         }
     }
 }
