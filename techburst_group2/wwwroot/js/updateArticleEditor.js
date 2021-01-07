ba5b156d0d5f60851f21d40ec5e12c6e038bca0b
Calin Peters <calinpeters98@hotmail.nl>
2021-01-07 13:21:05 +0100

    Added text editor to Edit page.

---
+++ techburst_group2/wwwroot/js/updateArticleEditor.js
@@ -0,0 +1,24 @@
+﻿let editorForm = document.querySelector('#edit');
+
+window.onload = function () {
+    let content = document.getElementById("articleContent").value;
+    let qEditor = quill;
+    render(content, qEditor.root);
+    //qEditor.root.innerHTML = content;
+};
+
+function getQuillContent() {
+    let qEditor = editors[editor];
+    let text = qEditor.root.textContent;
+    return text;
+}
+
+editorForm.onsubmit = function () {
+    var content = document.querySelector('input[name=content]');
+    content.value = getQuillContent();
+}
+
+var render = function (template, node) {
+    if (!node) return;
+    node.innerHTML = template;
+}
